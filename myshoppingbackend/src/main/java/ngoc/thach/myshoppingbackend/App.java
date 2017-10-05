cc97bd49011a61f117f2ac9581700ffb27ae98a9
lovebeat <lovebeat0124@gmail.com>
2017-10-05 09:31:12 +0700

    my project setup completed successfully

---
+++ myshoppingbackend/src/main/java/ngoc/thach/myshoppingbackend/App.java
@@ -0,0 +1,13 @@
+package ngoc.thach.myshoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
