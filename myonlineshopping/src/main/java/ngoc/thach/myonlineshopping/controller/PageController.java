393da5deb7774546f2f081f237b3ba82796a584a
lovebeat <lovebeat0124@gmail.com>
2017-10-07 15:06:18 +0700

    Hello

--- myonlineshopping/src/main/java/ngoc/thach/myonlineshopping/controller/PageController.java
+++ myonlineshopping/src/main/java/ngoc/thach/myonlineshopping/controller/PageController.java
@@ -8,12 +8,30 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PageController {
-	@RequestMapping(value= {"/","/index","/home"})
+	@RequestMapping(value = { "/", "/index", "/home" })
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting","welcome to web mvc" );
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
 		return mv;
 	}
+
+	@RequestMapping(value = "about")
+	public ModelAndView about() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "About");
+		mv.addObject("userClickAbout", true);
+		return mv;
+	}
+	
+	@RequestMapping(value = "contact")
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact");
+		mv.addObject("userClickContact", true);
+		return mv;
+	}
+
 //	@RequestMapping(value="/test")
 //	public ModelAndView test(@RequestParam(value="greeting", required=false) String greetingVal) {
 //		if (greetingVal==null) {
