5245345065d2475a710afe854f912e51799630ca
lovebeat <lovebeat0124@gmail.com>
2017-10-30 15:46:51 +0700

    a

--- myonlineshopping/src/main/webapp/assets/js/myapp.js
+++ myonlineshopping/src/main/webapp/assets/js/myapp.js
@@ -18,75 +18,83 @@ $(function() {
 	}
 	// code for jquery data table
 	// create dataset
-	var jsonUrl='';
-	console.log(window.contextRoot + '/json/data/category/' + window.categoryId + '/products');
-	if(window.categoryId==''){
-			jsonUrl = window.contextRoot + '/json/data/all/products';
-	}else{
-		jsonUrl = window.contextRoot + '/json/data/category/' + window.categoryId + '/products';
+	var jsonUrl = '';
+	console.log(window.contextRoot + '/json/data/category/' + window.categoryId
+			+ '/products');
+	if (window.categoryId == '') {
+		jsonUrl = window.contextRoot + '/json/data/all/products';
+	} else {
+		jsonUrl = window.contextRoot + '/json/data/category/'
+				+ window.categoryId + '/products';
 	}
 	var $table = $('#productListTable');
-	if($table.length){
-		
-		$table.DataTable({
-			lengthMenu: [[3,5,10,-1],['3 Records', '5 Records', '10 Records', 'All']],
-			pageLength: 5,
-			ajax: {
-				url: jsonUrl,
-				dataSrc: ''
-			},
-			columns: [
-				{
-					data: 'code',
-					mRender: function(data, type, row) {
-						return "<img class='dataTableImg' src='"+ window.contextRoot + "/resources/images/"+data+".jpg'>";
-					}
-				},
-				{
-					data: 'name'
-				},
-				{
-					data: 'brand'
-				},
-				{
-					data: 'unitPrice',
-					mRender: function(data, type,row) {
-						return '&#8377;' + data;
-					}
-				},
-				{
-					data: 'quantity',
-					mRender: function(data, type, row){
-						if(data<1){
-							return "<span style='color:red;'>Out of stock</span>"
-						}else{
-							return data;
-						}
-					}
-				},
-				{
-					data: 'id',
-					bSortable: false,
-					mRender: function(data, type,row){
-						var str= "";
-						str += '<a href="'+window.contextRoot+'/show/'+data+'/product" class="btn btn-primary"><span class="glyphicon glyphicon-eye-open"></span></a> &#160;';
-						if(row.quantity<1){
-							str += '<a href="jsavascript:void(0)" class="btn btn-success disabled"><span class="glyphicon glyphicon-shopping-cart"></span></a>';
-						}else{
-							str += '<a href="'+window.contextRoot+'/cart/add/'+data+'/product" class="btn btn-success"><span class="glyphicon glyphicon-shopping-cart"></span></a>';
-						}
-						
-						return str;
-					}
-				},
-			]
-		});
+	if ($table.length) {
+
+		$table
+				.DataTable({
+					lengthMenu : [ [ 3, 5, 10, -1 ],
+							[ '3 Records', '5 Records', '10 Records', 'All' ] ],
+					pageLength : 5,
+					ajax : {
+						url : jsonUrl,
+						dataSrc : ''
+					},
+					columns : [
+							{
+								data : 'code',
+								mRender : function(data, type, row) {
+									return "<img class='dataTableImg' src='"
+											+ window.contextRoot
+											+ "/resources/images/" + data
+											+ ".jpg'>";
+								}
+							},
+							{
+								data : 'name'
+							},
+							{
+								data : 'brand'
+							},
+							{
+								data : 'unitPrice',
+								mRender : function(data, type, row) {
+									return '&#8377;' + data;
+								}
+							},
+							{
+								data : 'quantity',
+								mRender : function(data, type, row) {
+									if (data < 1) {
+										return "<span style='color:red;'>Out of stock</span>"
+									} else {
+										return data;
+									}
+								}
+							},
+							{
+								data : 'id',
+								bSortable : false,
+								mRender : function(data, type, row) {
+									var str = "";
+									str += '<a href="'
+											+ window.contextRoot
+											+ '/show/'
+											+ data
+											+ '/product" class="btn btn-primary"><span class="glyphicon glyphicon-eye-open"></span></a> &#160;';
+									if (row.quantity < 1) {
+										str += '<a href="jsavascript:void(0)" class="btn btn-success disabled"><span class="glyphicon glyphicon-shopping-cart"></span></a>';
+									} else {
+										str += '<a href="'
+												+ window.contextRoot
+												+ '/cart/add/'
+												+ data
+												+ '/product" class="btn btn-success"><span class="glyphicon glyphicon-shopping-cart"></span></a>';
+									}
+
+									return str;
+								}
+							}, ]
+				});
 	}
 
 });
-
-
-
-
-
-
